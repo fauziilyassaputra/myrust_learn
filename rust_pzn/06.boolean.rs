4c96bec5efd3c9d95d9ef7846621e5035991bd79
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-05-18 14:04:47 +0700

    Create 06.boolean.rs

---
+++ rust_pzn/06.boolean.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[test]
+fn my_boolean(){
+    // default boolean is true
+    let a = true;
+    let b: bool = false;
+
+    println!("{} {}",a , b);
+}
+
+
