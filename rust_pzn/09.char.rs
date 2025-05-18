1e720617ab1326b23be4bfc255074dc9123e3866
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-05-18 14:40:38 +0700

    Create 09.char.rs

---
+++ rust_pzn/09.char.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[test]
+fn char_type(){
+  //  must use single quotation mark
+    let my_char1:char = 'a';
+    let my_char2:char = 'b';
+
+    println!("{} {}", my_char1, my_char2);
+}
