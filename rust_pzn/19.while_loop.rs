9b372221ffd2df5d8f1fcd2e14388c71c7f10a26
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-05-19 13:25:14 +0700

    Update 19.while_loop.rs

--- rust_pzn/19.while_loop.rs
+++ rust_pzn/19.while_loop.rs
@@ -13,3 +13,14 @@ fn while_loop(){
         counter += 1;
     }
 }
+
+#[test]
+fn while_loop2(){
+    let my_array: [&str; 5] = ["A","B","C","D","E"];
+    let mut index = 0;
+
+    while index < my_array.len() {
+        println!("value: {}", my_array[index]);
+        index += 1;
+    }
+}
