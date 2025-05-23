8a93b514affef30890841c2f1545b329d4a517fb
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-05-23 14:37:06 +0700

    Create 41.collection.rs

---
+++ rust_pzn/41.collection.rs
@@ -0,0 +1,17 @@
+
+
+fn main() {
+    println!("Hello, world!");
+}
+
+/*
+PERBANDINGAN :
+- jumlah data di collection bisa berkembang dan disimpan di heap
+- berbeda dengan array yang ukurannya tidak bisa dikembangkan dan disimpan didalam stack
+
+TIPE DATA COLLECTION :
+1. sequences, yaitu tipe data collection yang memiliki index
+2. maps, yaitu tipe data collection berupa key-value
+3. sets, yaitu tipe data collection unique yang tidak memiliki index
+*/
+
