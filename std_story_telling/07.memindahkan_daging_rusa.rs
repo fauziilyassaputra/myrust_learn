be6a7f9db561d3b9d55045699f667b9ba2e3ce6b
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-04 21:16:18 +0700

    Create 07.memindahkan_daging_rusa.rs

---
+++ std_story_telling/07.memindahkan_daging_rusa.rs
@@ -0,0 +1,26 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/* 
+- cerita :
+      sebelum tidur, kita berencana untuk berkemas-kemas agar besok bisa langsung melanjutkan perjalanan . aku akan membereskan
+    isi ransel.
+
+- masalah :
+      makanan yang ada didalam tas tidak terurut, urutkan!
+ 
+ */
+
+#[test]
+fn test2(){
+    // isi ranselnya
+    let mut isi_ransel = vec!["pisang","apel","daging rusa","pisang","pisang"];
+    
+    isi_ransel.swap(0,2);
+    
+    // cek isi ransel setelah durian busuk kita buang
+    println!("{:?}", isi_ransel);
+    
+    
+}
