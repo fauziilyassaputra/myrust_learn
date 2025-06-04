ebe5a9e79ac495d89985ec9501f4e7db557cf7e2
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-04 09:23:37 +0700

    Update and rename 01.hello_world.rs to 01.epilog.rs

---
+++ std_story_telling/01.epilog.rs
@@ -0,0 +1,14 @@
+fn main (){
+  println!("epilog ")
+}
+
+/* 
+selamat datang temanku, perkenalkan namaku adalah Ery.
+
+apakah kamu bersedia menemaniku dalam perjalanan menjalajahi dunia kepiting ? semoga perjalanan kita berjalan dengan
+lancar dan kita berhasil mendapatkan kekuatan kepiting sejati.
+
+kenapa kita harus mendapatkan kekuatan kepiting ? 
+
+karena kekuatan kepiting ini sangat penting dalam perjalanan kita selanjutnya nanti, soalnya kita akan menghadapi
+banyak sekali rintangan dan tangtangan bervariasi kedepannya.
