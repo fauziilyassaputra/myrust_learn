2ee9c90bc74afcd78fe1e39ad2c6944098d122b5
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-05 07:47:45 +0700

    Create 08.menemukan_apel.rs

---
+++ std_story_telling/08.menemukan_apel.rs
@@ -0,0 +1,31 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/* 
+- cerita:
+selain daging rusa,  kita juga mendapatkan apel . ayo masukkan ke ransel.
+
+- masalah :
+masukkan apel ke dalam ransel dengan apel lainnya
+ 
+ */
+
+#[test]
+fn test2(){
+    // isi ranselnya
+    let mut isi_ransel = vec!["daging rusa","apel","pisang","pisang","pisang"];
+    
+    isi_ransel.insert(1,"apel");
+    
+    // cek isi ransel setelah durian busuk kita buang
+    println!("{:?}", isi_ransel);
+    // output : ["pisang", "apel", "apel", "daging rusa", "pisang", "pisang"]
+    
+}
+
+/* 
+
+
+
+*/
