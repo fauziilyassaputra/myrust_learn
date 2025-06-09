8da626ea2236db1ed81b060729f9429049fa25c4
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-09 08:51:46 +0700

    Update and rename 15.a.rs to 15.hanya_melihat_jenis.rs

--- std_story_telling/15.hanya_melihat_jenis.rs
+++ std_story_telling/15.hanya_melihat_jenis.rs
@@ -11,14 +11,14 @@ fn main() {
 #[test]
 fn test(){
     // isi ransel
-    let mut isi_ransel = vec![0,1,2,3,3,4,5,6,6,7,2];
+    let mut isi_ransel = vec!["apel".to_string(),"apel".to_string(),"apel".to_string(),"jeruk".to_string(),"jeruk".to_string(),"pisang".to_string()];
     println!("isi ransel  sebelum : {:?}", isi_ransel);
 
 
-    // 
+    //  menghapus data yang sama
     isi_ransel.dedup();
     println!("isi ransel setelah : {:?}", isi_ransel)
-  //   isi ransel setelah : [0, 1, 2, 3, 4, 5, 6, 7, 2]
+  //   isi ransel setelah : ["apel", "jeruk", "pisang"]
 
 }
 
