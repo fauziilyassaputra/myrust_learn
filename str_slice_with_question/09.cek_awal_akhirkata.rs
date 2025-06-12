98f6d1a1412c7a023997ea31ab53c68a357d8f96
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-12 14:16:10 +0700

    Create 09.cek_awal_akhirkata.rs

---
+++ str_slice_with_question/09.cek_awal_akhirkata.rs
@@ -0,0 +1,33 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/* 
+ soal :
+ apakah awal kata pada variable learn dan learn 2 adalah "rust" ? , coba cek!
+ apakah akhir kata pada variable best dan best2 adalah "rust" ? coba cek!
+ */
+
+
+#[test]
+fn hilangkan_whitespace() {
+
+    let learn = "rust is the best language";
+    let best = "i like rust";
+    let cek_awal_kata = learn.starts_with("rust");
+    let cek_akhir_kata = best.ends_with("rust");
+    println!("{}", cek_awal_kata); //true
+    println!("{}", cek_akhir_kata);  //true
+    
+    let learn2 = "php is the best language";
+    let best2 = "i like php";
+    let cek_awal_kata2 = learn2.starts_with("rust");
+    let cek_akhir_kata2 = best2.ends_with("rust");
+    println!("{}", cek_awal_kata2); //false
+    println!("{}", cek_akhir_kata2);  //false
+   
+}
+/*
+
+
+ */
