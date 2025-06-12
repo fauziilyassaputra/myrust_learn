d28aa032afd6b892a0daa56c9ab28853cb3ae41f
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-12 15:24:07 +0700

    Create 01.prolog.rs

---
+++ vecDeque_with_question/01.prolog.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/* 
+https://doc.rust-lang.org/std/collections/struct.VecDeque.html# 
+
+perbedaan utama antara vecdeque dengan vec adalah vecdeque bisa mengolah data awal dan akhirnya sehingga lebih efisien
+dan cocok untuk structut data seperti antrian  / FIFO
+
+*/
+
+
