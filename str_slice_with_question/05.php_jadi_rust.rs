aa40d05fb9029d56693b4aaf1e8c280ebdd92ff2
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-12 09:55:11 +0700

    Create 05.php_jadi_rust.rs

---
+++ str_slice_with_question/05.php_jadi_rust.rs
@@ -0,0 +1,29 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/* 
+ soal :
+   ubah kata "php" menjadi "rust" pada variable like menjadi like_rust !
+ 
+ */
+
+
+#[test]
+fn ganti_teks() {
+    // ubah semua kata php dengan rust
+    let like = "i like php language, php is the best";
+    let like_rust = like.replace("php", "rust");
+    println!("{}", like_rust);
+
+     // ubah hanya awal kata php menjadi rust
+    let info = "php is a good choice, php is elephant language";
+    let info_rust = info.replacen("php", "rust", 1);
+    println!("{}", info_rust)
+  
+}
+/*
+replace berguna untuk menggantikan substring pada string. replace mengganti semua substring lama dengan yang baru,
+berbeda dengan replacen yang hanya menggantikan sejumlah substring sesuai yang di tentukan
+
+ */
