9a6a16e36188c82b310e57b81d2bacf509ec8e30
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-14 20:27:59 +0700

    Create 01.prolog.rs

---
+++ hashmap_btreemap_with_question/01.prolog.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/* 
+ Hash map reference : https://doc.rust-lang.org/std/collections/struct.HashMap.html
+ BTree map : https://doc.rust-lang.org/std/collections/struct.BTreeMap.html
+  
+ - gunakan hashmap jika membutuhkan pencarian cepat tanpa peduli urutan elemen
+ - gunakan btreemap  jiks perlu menyimpan data yang terorganisir, namun lebih lambat dibanding hashmap
+ 
+ */
+
