9e7f03535c4b56c6fdb2f6df1fb909590c0dc1b0
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-14 16:35:18 +0700

    Update and rename prolog.rs to 01.prolog.rs

---
+++ linkedlist_with_question/01.prolog.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/* 
+ reference : https://doc.rust-lang.org/std/collections/struct.LinkedList.html#implementations
+
+ gunakan "use std::collections::LinkedList;" untuk menggunakan linkedlist pada rust
+ 
+ */
