0c74e6b50fd21a6102900a5cfc41555865ce2a51
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-14 21:48:13 +0700

    Create 03.cek_isi_hashmap.rs

---
+++ hashmap_btreemap_with_question/03.cek_isi_hashmap.rs
@@ -0,0 +1,37 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/* 
+ 
+ 
+ */
+use std::collections::HashMap;
+// use std::collections::BTreeMap;
+
+#[test]
+fn test_linked_list() { 
+    // membuat BTreeMap 
+    let  my_hashmap: HashMap<i32, &str>  = HashMap::from([
+        (1,"hoshimi miyabi"),
+        (2,"asaba harumasa"),
+        (3,"tsukisiro yanagi"),
+        (4,"shokaku")
+    ]); 
+    
+    // cek semua isi value  pada  variable my_hashmap
+    for val in my_hashmap.values() {
+        println!("{val}")
+    }
+    
+    // cek semua isi keys pada variable my_hashmap
+    for key in my_hashmap.keys() {
+        println!("{key}")
+    }
+   
+}
+/*
+penjelasan :
+   
+
+ */
